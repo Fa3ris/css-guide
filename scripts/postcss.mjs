0ac9c349a2464f888c36cdc65834a48b80331970
Fabrice Tsiava <fabrice.tsiava@contentsquare.com>
2023-11-12 19:31:29 +0100

    rewrite async/await

--- scripts/postcss.mjs
+++ scripts/postcss.mjs
@@ -1,49 +1,28 @@
-import postcss from "postcss";
-import postcssFocus from "postcss-focus";
-import autoprefixer from "autoprefixer";
-import cssnanoPlugin from "cssnano";
-import postcssModules from "postcss-modules";
-import path from "path";
 import { readFile, writeFile } from "fs/promises";
+import path from "path";
+import postcss from "postcss";
+import postcssModules from "postcss-modules";
 
 import { globSync } from "glob";
 
-const inFile = path.resolve("src/styles/style.css");
+async function toCssModules() {
+  const processor = postcss([postcssModules]);
 
-const outFile = path.resolve("public/style.css");
+  const paths = globSync("src/**/*.css");
 
-const OLD_FOCUS = false;
+  const outFile = path.resolve("public/style.css");
 
-const paths = globSync("src/**/*.css");
+  const allCss = await Promise.all(
+    paths.map(async (p) => {
+      const from = path.resolve(p);
+      const content = await readFile(from);
+      const result = await processor.process(content, { from, outFile });
+      return result.css;
+    })
+  );
 
-const plugins = [
-  //   postcssFocus({ oldFocus: OLD_FOCUS }),
-  //   autoprefixer({ flexbox: true }),
-  postcssModules,
-  //   cssnanoPlugin,
-];
+  await writeFile(outFile, allCss.join("\n"));
+  console.log("finish process CSS");
+}
 
-const processor = postcss(plugins);
-
-const allP = Promise.all(
-  paths.map((p) => {
-    const from = path.resolve(p);
-    return readFile(from).then((c) => processor.process(c, { from, outFile }));
-  })
-);
-
-allP.then((p) => {
-  const css = p.map((e) => e.css).join("\n");
-  writeFile(path.resolve("public/style.css"), css).then(() => {
-    console.log("finish process CSS");
-  });
-});
-
-// readFile(inFile).then((c) => {
-//   processor.process(c, { from: inFile, to: outFile }).then((out) => {
-//     console.log(out);
-//     writeFile(out.opts.to, out.css);
-//   });
-// });
-
-// console.log(paths);
+toCssModules();
